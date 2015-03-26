3ebe5808c1032f4f4e7875ff7b14f11486d94598
Travis Pynenburg <t.pynenburg@TRPYNENBURG-MAC.local>
2015-03-26 19:17:24 -0400

    I added chicken

---
+++ src/headfirst/designpatterns/adapter/ducks/LeghornChicken.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package headfirst.designpatterns.adapter.ducks;
+
+/**
+ *
+ * @author t.pynenburg
+ */
+public class LeghornChicken implements chicken {
+    
+    
+}
